39b44d3d9fcfd22aa4da6a4b61ef91b2770d9c5f
opixxx <opix0306@naver.com>
2024-01-18 12:34:08 +0900

    init

---
+++ src/main/java/com/example/carrotdiary/CarrotdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrotdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotdiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrotdiary/CarrotdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrotdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
