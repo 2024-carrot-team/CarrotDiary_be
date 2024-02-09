a90e4d7d9b9387c319d0a3c0da592eddb2b85645
naraspc <wodnrtkfka@naver.com>
2024-02-09 19:16:58 +0900

    feat : add MemberController CRUD function
    회원가입 기능 추가
    
    추후 로그인 및 토큰기능 구현 후 토큰파싱으로 수정예정

--- src/main/java/com/example/carrrotdiary/member/service/MemberService.java
+++ src/main/java/com/example/carrrotdiary/member/service/MemberService.java
@@ -6,15 +6,13 @@ import com.example.carrrotdiary.member.dto.MemberResponseDto;
 import com.example.carrrotdiary.member.entity.MemberDetails;
 import com.example.carrrotdiary.member.entity.MemberEntity;
 import com.example.carrrotdiary.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +25,7 @@ public class MemberService implements UserDetailsService {
     private JwtUtils jwtUtil;
 
     //C
-    public MemberResponseDto createMember(MemberRequestDto memberRequestDto) {
+    public void createMember(MemberRequestDto memberRequestDto) {
         MemberEntity memberEntity = MemberEntity.builder()
                 .email(memberRequestDto.getEmail())
                 .password(memberRequestDto.getPassword())
@@ -37,7 +35,9 @@ public class MemberService implements UserDetailsService {
                 .build();
 
         memberRepository.save(memberEntity);
-        return MemberResponseDto.fromEntity(memberEntity);
+        MemberResponseDto.fromEntity(memberEntity);
+
+
     }
 
 
@@ -49,38 +49,30 @@ public class MemberService implements UserDetailsService {
     }
 
     //R 단건조회
-    public MemberResponseDto checkMemberDetails(Long id) {
-        MemberEntity member = memberRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("doesn't exist username"));
+    public MemberResponseDto checkMemberDetails(String email) throws UsernameNotFoundException{
+        MemberEntity member = memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("Member cannot be found"));
 
         return MemberResponseDto.fromEntity(member);
     }
 
 
     //U 수정
-    public MemberResponseDto updateMember(Long id, MemberRequestDto.updateRequestDto updateRequestDto) {
-        MemberEntity member = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("can not find member"));
+    public void updateMember(String email, MemberRequestDto.updateRequestDto updateRequestDto) {
+        MemberEntity member = memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("can not find member"));
 
         member.updateMember(updateRequestDto);
 
-        return MemberResponseDto.fromEntity(member);
+        MemberResponseDto.fromEntity(member);
     }
 
     //D 삭제
 
-    public Boolean deleteMember(Long id) {
+    public void deleteMember(String email) {
 
-        if (checkEntityExist(id)) {
-            memberRepository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
+        MemberEntity member = memberRepository.findByEmail(email).orElseThrow(() ->new EntityNotFoundException("entity not found"));
 
-
+        memberRepository.delete(member);
     }
 
-    private Boolean checkEntityExist(Long id) {
-        Optional<MemberEntity> member = memberRepository.findById(id);
-        return member.isPresent();
-    }
+
 }
