bacf24a347de967ff7159f06805b1d6f4d783425
opixxx <opix0306@naver.com>
2024-02-13 20:29:08 +0900

    feat: AWS S3 의존성 추가

--- src/test/java/com/example/carrotdiary/CarrotDiaryApplicationTests.java
+++ src/test/java/com/example/carrotdiary/CarrotDiaryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarrotdiaryApplicationTests {
+class CarrotDiaryApplicationTests {
 
 	@Test
 	void contextLoads() {
