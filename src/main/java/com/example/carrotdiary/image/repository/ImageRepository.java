985deceb7cb5f4193da04dc303035316162ee20a
opixxx <opix0306@naver.com>
2024-03-10 12:59:01 +0900

    style : 사용하지 않는 메서드 삭제

--- src/main/java/com/example/carrotdiary/image/repository/ImageRepository.java
+++ src/main/java/com/example/carrotdiary/image/repository/ImageRepository.java
@@ -1,18 +1,8 @@
 package com.example.carrotdiary.image.repository;
 
 import com.example.carrotdiary.image.entity.Image;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
-
-    Optional<Image> findByImageUrl(String imageUrl);
-
-    @Query("select i.id from Image i where i.diary.id = :diaryId")
-    List<Long> findImageIdsByDiaryId(@Param("diaryId") Long diaryId);
-
 
 }
