6b1bafc796d548fc434c0b6a7eb171d2c5bfc6f5
naraspc <wodnrtkfka@naver.com>
2024-03-28 22:31:24 +0900

    feat : add member Image
    회원엔티티 사진 필드 추가
    
    서비스 & 컨트롤러 로직 수정예정

--- src/main/java/com/example/carrotdiary/image/entity/Image.java
+++ src/main/java/com/example/carrotdiary/image/entity/Image.java
@@ -4,6 +4,7 @@ import static jakarta.persistence.FetchType.LAZY;
 
 import com.example.carrotdiary.diary.entity.Diary;
 import com.example.carrotdiary.global.common.BaseEntity;
+import com.example.carrotdiary.member.entity.Member;
 import com.example.carrotdiary.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -66,6 +67,15 @@ public class Image extends BaseEntity {
     public void updateImage(String fileName, String imageUrl) {
         this.fileName = fileName;
         this.imageUrl = imageUrl;
+    }
+
+    public static Image addUserImage(String imageUrl, String fileName, Member member) {
+        Image image = new Image();
+        image.imageUrl = imageUrl;
+        image.fileName = fileName;
+        image.setMember(member);
+
+        return image;
     }
 
 
