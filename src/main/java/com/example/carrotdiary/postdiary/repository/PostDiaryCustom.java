98fc6da1efc4c50e675a26b1601bfca23397eaec
opixxx <opix0306@naver.com>
2024-04-17 14:14:23 +0900

    refactor: PostDiary 도메인 관련 로직 Querydsl 적용

---
+++ src/main/java/com/example/carrotdiary/postdiary/repository/PostDiaryCustom.java
@@ -0,0 +1,13 @@
+package com.example.carrotdiary.postdiary.repository;
+
+import com.example.carrotdiary.diary.entity.DiarySearch;
+import com.example.carrotdiary.postdiary.entity.PostDiary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PostDiaryCustom {
+
+    Page<PostDiary> findAllPostDiariesPaging(Pageable pageable);
+
+    Page<PostDiary> findPostDiaryBySearch(DiarySearch diarySearch, String searchContent, Pageable pageable);
+}
